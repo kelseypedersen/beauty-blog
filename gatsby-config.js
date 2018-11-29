6c01018c784428638430243cfb83f5bd38a8a542
Kelsey Pedersen <kelsey.pedersen@stitchfix.com>
2018-11-29 08:50:38 +0100

    Add gatsby manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,14 +20,29 @@ if (!spaceId || !accessToken) {
 }
 
 module.exports = {
+  siteMetadata: {
+    title: 'Blog by Kelsey',
+  },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Blog',
+        short_name: 'Blog',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
+    'gatsby-transformer-remark',
   ],
 }
